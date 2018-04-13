a1d609d033500c3342d8ffda14c2732cb909e2cd
Nam Do <sirsunday89@gmail.com>
2018-04-13 08:43:35 -0400

    3rd:Website page flexboxes and styling

--- websites.php
+++ websites.php
@@ -2,8 +2,49 @@
 define ("TITLE", "Team | Nam's Portfolio");
  include ('includes/header.php');
  ?>
-
-    <main>
-
-    </main>
+<main id="main1">
+  <h1>WEBSITES</h1>
+  <div id="flexBox1">
+    <div id="webBox1" class="websiteContainers">
+      <h4>Copper Moon</h4>
+      <figure>
+        <img src="images/CPimg.PNG"/>
+      </figure>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan tristique mi, eget sodales leo tincidunt at. Phasellus sapien risus, efficitur quis sodales in, euismod non lectus. Etiam eget lobortis leo. Nullam bibendum id sapien et suscipit. Praesent vel mauris facilisis, sodales elit a, convallis magna. Maecenas quis orci id nisl finibus condimentum. Praesent in sagittis orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas hendrerit dui augue, et viverra odio rutrum ac. Vestibulum quis arcu vitae justo molestie placerat non a massa.
+      </p>
+      <div id="hoverDiv">
+          <a>The Process</a>
+      </div>
+    </div>
+    <div id="webBox2" class="websiteContainers">
+      <h4>North Shore Safety</h4>
+      <figure>
+        <img src="images/NSSimg.PNG"/>
+      </figure>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan tristique mi, eget sodales leo tincidunt at. Phasellus sapien risus, efficitur quis sodales in, euismod non lectus. Etiam eget lobortis leo. Nullam bibendum id sapien et suscipit. Praesent vel mauris facilisis, sodales elit a, convallis magna. Maecenas quis orci id nisl finibus condimentum. Praesent in sagittis orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas hendrerit dui augue, et viverra odio rutrum ac. Vestibulum quis arcu vitae justo molestie placerat non a massa.
+      </p>
+      <div id="hoverDiv">
+      </div>
+      <div id="hoverDiv">
+          <a>The Process</a>
+      </div>
+    </div>
+    <div id="webBox3" class="websiteContainers">
+      <h4>Electric Air</h4>
+      <figure>
+        <img src="images/EAimg.jpg"/>
+      </figure>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan tristique mi, eget sodales leo tincidunt at. Phasellus sapien risus, efficitur quis sodales in, euismod non lectus. Etiam eget lobortis leo. Nullam bibendum id sapien et suscipit. Praesent vel mauris facilisis, sodales elit a, convallis magna. Maecenas quis orci id nisl finibus condimentum. Praesent in sagittis orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas hendrerit dui augue, et viverra odio rutrum ac. Vestibulum quis arcu vitae justo molestie placerat non a massa.
+      </p>
+      <div id="hoverDiv">
+      </div>
+      <div id="hoverDiv">
+          <a>The Process</a>
+      </div>
+    </div>
+  </div>
+</main>
 <?php include ('includes/footer.php');?>
