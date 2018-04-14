7a4030541b60fd3909537632da0cb14a3f85f1a2
Nam Do <sirsunday89@gmail.com>
2018-04-13 21:50:01 -0400

    add website-template.php and updated arrays.php

--- includes/arrays.php
+++ includes/arrays.php
@@ -15,6 +15,7 @@
     ),
   );
 
+//Web features on home page
   $webFeatures=array(
     array(
       header    =>"Responsive Design",
@@ -35,7 +36,7 @@
         orci luctus et"
     ),
   );
-
+//Photo features on home page
   $photoFeatures=array(
     array(
       header    =>"Breath Taking Quality",
@@ -56,4 +57,99 @@
         orci luctus et"
     ),
   );
+
+  //websites info display on website.php
+    $websListing=array(
+      array(
+        header      =>"The Copper Moon",
+        color       =>"#D26A4A",
+        description =>"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+         Fusce facilisis turpis felis. Vestibulum ante ipsum primis in faucibus
+          orci luctus et",
+        display     =>"CMpic1",
+        background  =>"CMpic2",
+        templateKey =>"CM0"
+        ),
+        array(
+          header      =>"North Shore Safety",
+          color       =>"#000000",
+          description =>"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+           Fusce facilisis turpis felis. Vestibulum ante ipsum primis in faucibus
+            orci luctus et",
+          display     =>"NSSpic1",
+          background  =>"NSSpic2",
+          templateKey =>"NSS1"
+          ),
+          array(
+            header      =>"Electric Air",
+            color       =>"#000000",
+            description =>"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+             Fusce facilisis turpis felis. Vestibulum ante ipsum primis in faucibus
+              orci luctus et",
+            display     =>"EApic1",
+            background  =>"EApic2",
+            templateKey =>"EA2"
+            ),
+    );
+    //website info for website-template.php
+$websDetails=array(
+  "CM0" => array(
+      title =>"The Copper Moon - Static Website",
+      photo1=>"CMpic3",
+      photo2=>"CMpic4",
+      photo3=>"CMpic5",
+      photo4=>"CMpic6",
+      description1=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+       Fusce facilisis turpis felis. Vestibulum ante ipsum primis in faucibus
+        orci luctus et",
+      description2=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+       Fusce facilisis turpis felis. Vestibulum ante ipsum primis in faucibus
+        orci luctus et",
+      description3=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+       Fusce facilisis turpis felis. Vestibulum ante ipsum primis in faucibus
+        orci luctus et",
+      description4=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+       Fusce facilisis turpis felis. Vestibulum ante ipsum primis in faucibus
+        orci luctus et"
+  ),
+  "NSS1" => array(
+      title =>"North Shore Safety - Wordpress Website",
+      photo1=>"NSSpic3",
+      photo2=>"NSSpic4",
+      photo3=>"NSSpic5",
+      photo4=>"NSSpic6",
+      description1=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+       Fusce facilisis turpis felis. Vestibulum ante ipsum primis in faucibus
+        orci luctus et",
+      description2=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+       Fusce facilisis turpis felis. Vestibulum ante ipsum primis in faucibus
+        orci luctus et",
+      description3=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+       Fusce facilisis turpis felis. Vestibulum ante ipsum primis in faucibus
+        orci luctus et",
+      description4=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+       Fusce facilisis turpis felis. Vestibulum ante ipsum primis in faucibus
+        orci luctus et"
+  ),
+  "EA2" => array(
+      title =>"Electric Air - Ecommerce Website",
+      photo1=>"EApic3",
+      photo2=>"EApic4",
+      photo3=>"EApic5",
+      photo4=>"EApic6",
+      description1=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+       Fusce facilisis turpis felis. Vestibulum ante ipsum primis in faucibus
+        orci luctus et",
+      description2=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+       Fusce facilisis turpis felis. Vestibulum ante ipsum primis in faucibus
+        orci luctus et",
+      description3=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+       Fusce facilisis turpis felis. Vestibulum ante ipsum primis in faucibus
+        orci luctus et",
+      description4=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+       Fusce facilisis turpis felis. Vestibulum ante ipsum primis in faucibus
+        orci luctus et"
+      ),
+    );
+
  ?>
